36103a0455b594f5f42dac9694e886a0551019db
HighOptions <63575863+HighOptions@users.noreply.github.com>
2023-10-27 00:13:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+
+let popup=document.getElementById("popup");
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+
+    sections.forEach(section => {
+        section.addEventListener('click', function() {
+            section.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
+
+
+const icons = document.querySelectorAll('.clickable-icon');
+
+    icons.forEach(icon => {
+        icon.addEventListener('click', function () {
+            const url = this.getAttribute('href');
+            window.location.href = url;
+        });
+    });
+
+
+    const navbar = document.querySelector('.navbar');
+    const section1 = document.querySelector('#page1'); // Adjust based on your content structure
+    
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > section1.offsetTop) {
+            navbar.style.backgroundColor = '#B2C8BA'; // Change to your desired background color
+        } else {
+            navbar.style.backgroundColor = 'transparent'; // Reset to transparent background
+        }
+    });
+    
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const whiteSection = document.querySelector('#page1');
+        const blackSection = document.querySelector('#page2');
+    
+        window.addEventListener('scroll', function() {
+            const scrollPosition = window.scrollY;
+    
+            // Calculate the transition point
+            const transitionPoint = whiteSection.offsetHeight - 100;
+    
+            // Update background colors based on scroll position
+            if (scrollPosition >= transitionPoint) {
+                whiteSection.style.backgroundColor = 'transparent';
+                blackSection.style.backgroundColor = 'black';
+            } else {
+                whiteSection.style.backgroundColor = 'white';
+                blackSection.style.backgroundColor = 'transparent';
+            }
+        });
+    });
+    
+    
+
+    $(document).ready(function () {
+        // Smooth scrolling for navigation links and buttons
+        $(".nav-links a, .scroll-button, .button-container a").on("click", function (event) {
+          if (this.hash !== "") {
+            event.preventDefault();
+      
+            var hash = this.hash;
+      
+            $("html, body").animate(
+              {
+                scrollTop: $(hash).offset().top,
+              },
+              800, // Scroll speed in milliseconds
+              function () {
+                window.location.hash = hash;
+              }
+            );
+          }
+        });
+      });
+      
